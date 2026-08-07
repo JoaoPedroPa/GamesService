9e9c3cd52284c6e96ec6fe5ba2cf6a0c311dea20
Joao Pedro <joaopedro.contato829@gmail.com>
2026-08-07 15:27:45 -0300

    update teste10

--- FCG.Games.Application/Games/GameService.cs
+++ FCG.Games.Application/Games/GameService.cs
@@ -9,7 +9,7 @@ public sealed record CreateGameRequest(string Name, string Genre, decimal Price)
 
 
 
-// teste CI GitHub Actions teste9
+// teste CI GitHub Actions teste10
 public sealed record PurchaseGameResponse(
     Guid PurchaseId,
     Guid PaymentId,
